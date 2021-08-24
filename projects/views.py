f7209117d6f1e51495068d85b250bc5422fd32a0
active-sludge <hcdayan@hotmail.com>
2021-08-24 16:53:47 +0300

    Add basics. Add Database.

--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
+from .forms import ProjectForm
 
 
 def projects(request):
-    return HttpResponse('Here are our projects.')
+    all_projects = Project.objects.all()
+    context = {'projects': all_projects}
+    return render(request, 'projects/projects.html', context=context)
 
 
 def project(request, pk):
-    return HttpResponse('Here is a single project no ' + str(pk))
+    projectObject = Project.objects.get(id=pk)
+    tags = projectObject.tags.all()
+    return render(request, 'projects/single-project.html', {'project': projectObject, 'tags': tags})
+
+
+def create_project(request):
+    form = ProjectForm()
+    context = {'form': form}
+    return render(request, 'projects/project_form.html', context)
